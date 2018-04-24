4c53ba99caead4d970af0deccefc3182d9690ba3
Shubham Gupta <shubham.gupta@coviam.com>
2018-04-24 14:27:58 +0530

    Adding Build Process

--- src/main/java/com/shubham/zest_money/Application.java
+++ src/main/java/com/shubham/zest_money/Application.java
@@ -17,8 +17,8 @@ public class Application {
 
   public static void main(String[] args) {
     gameInfo();
-    //GamePlay gamePlay = new GamePlay(scanner);
-    //gamePlay.start();
+    GamePlay gamePlay = new GamePlay(scanner);
+    gamePlay.start();
   }
 
   private static void gameInfo() {
